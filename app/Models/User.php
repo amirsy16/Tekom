0b9459bcd888c5c72c97afeb8321378f461ed746
Amir Syofian <amirsyofia2@gmail.com>
2025-11-20 03:08:41 +0700

    user.php v2

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,7 +54,7 @@ class User extends Authenticatable implements FilamentUser
 
       public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
     }
     // Relationships
     public function organization(): BelongsTo
