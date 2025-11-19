3583771448a4b1d585cbe389e9535354302c6f0e
Amir Syofian <amirsyofia2@gmail.com>
2025-11-20 01:45:37 +0700

    build assets

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ use App\Http\Controllers\MapController;
 Route::redirect('/', '/admin');
 
 Route::get('/api/tower-locations', [MapController::class, 'towerLocations'])->name('api.tower-locations');
+
