e64b4e6a6a99e026dd7c8827b744c663b9871550
Amir Syofian <amirsyofia2@gmail.com>
2025-11-20 01:36:26 +0700

    fix 403

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MapController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin');
 
 Route::get('/api/tower-locations', [MapController::class, 'towerLocations'])->name('api.tower-locations');
